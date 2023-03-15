04b259b98a1d09e39a7f1a4d9d0bdf917b9d43a9
Stella Mazzillo <stellagress5@gmail.com>
2023-03-15 02:05:26 -0400

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+
+//1-
+let num1=31;
+let num2=2;
+const multiply = num1 * num2;
+
+
+//2-
+const random = Math.ceil(Math.random()*10);
+
+
+//3-
+let num3 = 20;
+let num4 = 8;
+const mod = num3 % num4;
+
+
+//4-
+const max = Math.max(1,2,3,4,5,20);
+
+
+
+
+
+
+
+
+
+
